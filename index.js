ce19318b22fb48c7d54f9ea7d7559ec6b1826d27
reSHARMA <reshabhsh@gmail.com>
2017-01-05 04:22:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ app.get("/", function(request, response) { response.send("Hello, world!");
                                          });
 
   
-
-app.listen(process.env.PORT,function(){
-  console.log("started listening on port " + process.env.PORT);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
 
