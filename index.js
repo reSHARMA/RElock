3b478edef7e4137fd0c524fb23e28e34df3ac10e
reSHARMA <reshabhsh@gmail.com>
2017-01-05 04:26:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-var express = require("express"); 
+var express = require("express");
 var app = express();
 
-// let's see wether this thing works or not :P
-
-app.get("/", function(request, response) { response.send("Hello, world!");
-                                         });
-
-  
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+// Set up a URL route
+app.get("/", function(req, res) {
+ res.send("Heroku Demo!");
 });
 
+// bind the app to listen for connections on a specified port
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+// Render some console log output
+console.log("Listening on port " + port);
