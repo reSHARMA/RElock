bcda32302c16312388f6368292af8311d01c2efa
reSHARMA <reshabhsh@gmail.com>
2017-01-18 08:14:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ io.on('connection', function(client) {
 client.emit("client","connection successfully established");
 	    io.on('connection',function(pi){
 		    console.log("pi connected ... ");
-	    	pi.emit("status",'test');
+	    	pi.emit("stat",'test');
 			client.emit('remote','connected successfully ');
 		
 	    });
